4f10f95089bdfb2dc174eb4f9a169bd7e7f16cca
Blade Barringer <blade@crookedneighbor.com>
2015-10-02 15:56:25 -0500

    Add endpoint global

--- test/helper.js
+++ test/helper.js
@@ -9,7 +9,9 @@ global.nock = nock;
 global.expect = chai.expect;
 global.sandbox = sinon.sandbox.create();
 
-afterEach(function () {
+global.endpoint = 'https://habitica.com/api/v2';
+
+afterEach(() => {
   global.sandbox.restore();
 });
 
