0598d9aad40aba0dc266c242d63692946bb33aa0
Blade Barringer <blade@crookedneighbor.com>
2015-10-04 10:06:07 -0500

    Remove console warning

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import Content from './content';
 
 export default class {
   constructor (options = {}) {
-    if (!(options.uuid && options.token)) {
-      console.warn('Missing credentials; Only content routes will be available'); // eslint-disable-line no-console
-    }
-
     this._connection = new Connection({
       uuid: options.uuid,
       token: options.token,
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,27 +1,12 @@
 import Habitica from '../../src/index';
+import Connection from '../../src/connection';
 
 describe('Habitica Api', () => {
-
-  describe('initialization', () => {
-    beforeEach(() => {
-      sinon.stub(console, 'warn');
-    });
-
-    afterEach(() => {
-      console.warn.restore(); // eslint-disable-line no-console
-    });
-
-    it('warns if user id and api are not provided', () => {
-      let api = new Habitica();
-
-      expect(console.warn).to.be.calledOnce; // eslint-disable-line no-console
-      expect(console.warn).to.be.calledWith('Missing credentials; Only content routes will be available'); // eslint-disable-line no-console
-    });
-  });
 
   describe('attributes', () => {
     let api = new Habitica({uuid: 'foo', token: 'bar' });
     let attributes = [
+      'account',
       'content',
     ];
 
