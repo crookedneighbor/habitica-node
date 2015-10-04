ae102414167f0d2655628de63c47f3e137a06f49
Blade Barringer <blade@crookedneighbor.com>
2015-10-03 21:49:22 -0500

    Adjust tests to be simpler

--- test/support/travis_init.js
+++
@@ -1,9 +0,0 @@
-import {sync as glob} from 'glob';
-import {each} from 'lodash';
-let testFiles = glob('./test/*.js');
-
-require('../../../habitrpg/website/src/server.js');
-
-each(testFiles, (file) => {
-  require(`../../${file}`);
-});
