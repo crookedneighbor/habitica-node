fd6b2a45753c3ddef44c650ac6e036fe3b391dba
Blade Barringer <blade@crookedneighbor.com>
2015-10-09 22:16:14 -0500

    Create additional test tasks

--- tasks/gulp-test.js
+++ tasks/gulp-test.js
@@ -25,10 +25,30 @@ gulp.task('test:integration', ['test:prepare'], () => {
   return runTests(tests);
 });
 
+gulp.task('test:integration:noExit', ['test:prepare'], () => {
+  let tests = glob('./test/integration/**/*.js');
+
+  return runTests(tests, true);
+});
+
+gulp.task('test:integration:watch', ['test:integration:noExit'], () => {
+  gulp.watch(['src/**', 'test/integration/**'], ['test:integration:noExit']);
+});
+
 gulp.task('test:unit', () => {
   let tests = glob('./test/unit/**/*.js');
 
   return runTests(tests);
+});
+
+gulp.task('test:unit:noExit', ['test:prepare'], () => {
+  let tests = glob('./test/unit/**/*.js');
+
+  return runTests(tests, true);
+});
+
+gulp.task('test:unit:watch', ['test:unit:noExit'], () => {
+  gulp.watch(['src/**', 'test/unit/**'], ['test:unit:noExit']);
 });
 
 gulp.task('test:prepare', ['test:dropDB', 'test:startHabitica']);
@@ -48,7 +68,10 @@ gulp.task('test:startHabitica', ['test:dropDB'], (done) => {
   server.listen(process.env.PORT, done);
 });
 
-function runTests(source) {
-  return gulp.src(source)
-    .pipe(mocha(MOCHA_CONFIG));
+function runTests(source, noExit) {
+  return gulp.src(source, { read: false })
+    .pipe(mocha(MOCHA_CONFIG))
+    .on('end', () => {
+      if (!noExit) process.exit();
+    });
 }
