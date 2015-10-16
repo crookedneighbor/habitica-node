e214a8d15487d6b3671fcdee61202b840518f002
Blade Barringer <blade@crookedneighbor.com>
2015-10-16 11:57:33 -0500

    Update 2015-10-16T16:57:31.974Z

--- tasks/gulp-lint.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import eslint from 'gulp-eslint';
-
-gulp.task('lint', () => {
-  return gulp.src(['src/**/*.js', 'test/**/*.js', 'tasks/**/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
--- test/support/globals.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-import sinon from 'sinon';
-
-chai.use(require('chai-as-promised'));
-chai.use(require('sinon-chai'));
-
-global.sinon = sinon;
-global.expect = chai.expect;
-global.endpoint = 'https://habitica.com/api/v2';
