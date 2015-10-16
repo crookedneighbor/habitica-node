d28e05dbc6cedb85fe6323b35ec086e6846cb948
Blade Barringer <blade@crookedneighbor.com>
2015-10-16 11:33:44 -0500

    Use docco directly in gulp

--- tasks/gulp-docs.js
+++ tasks/gulp-docs.js
@@ -1,14 +1,15 @@
 import gulp from 'gulp';
-import {exec} from 'child_process';
+import {sync as glob} from 'glob';
+import docco from 'docco';
 
 gulp.task('docs', (done) => {
-  exec('node_modules/.bin/docco --template docs/assets/template.jst --css docs/assets/css/style.css src/*', (err, stdout, stderr) => {
-    if (err) return done(err);
-    if (stdout) console.log(stdout); // eslint-disable-line no-console
-    if (stderr) console.error(stderr); // eslint-disable-line no-console
+  let options = {
+    template: 'docs/assets/template.jst',
+    css: 'docs/assets/css/style.css',
+    args: glob('src/**/*.js'),
+  };
 
-    done();
-  });
+  docco.document(options, done);
 });
 
 gulp.task('docs:watch', () => {
