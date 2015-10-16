bee6c7ab5f441affece791505e321cb556dcc065
Blade Barringer <blade@crookedneighbor.com>
2015-10-16 12:03:34 -0500

    Add docs as a dependency for gh-pages task

--- tasks/gulp-ghpages.js
+++ tasks/gulp-ghpages.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import {sync as glob} from 'glob';
 import ghPages from 'gulp-gh-pages';
 
-gulp.task('gh-pages', () => {
+gulp.task('gh-pages', ['docs'], () => {
   return gulp.src('./docs/**/*')
   .pipe(ghPages());
 });
