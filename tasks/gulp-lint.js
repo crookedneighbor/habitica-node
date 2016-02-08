477bb00c9150e8eac63d8515f40a6235f53feb26
Blade Barringer <blade@crookedneighbor.com>
2016-02-07 20:40:41 -0600

    refactor: Remove gulp-eslint dependency

--- tasks/gulp-lint.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-
-gulp.task('lint', () => {
-  return gulp.src(['src/**/*.js', 'test/**/*.js', 'tasks/**/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
