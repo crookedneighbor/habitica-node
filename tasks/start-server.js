7e889d4cdbbcbd872a8f700c6e8d457aab2b7cbc
Blade Barringer <blade@crookedneighbor.com>
2016-05-23 08:26:52 -0500

    feat: Expose env variables when starting server

--- tasks/start-server.js
+++ tasks/start-server.js
@@ -13,6 +13,8 @@ if (args.length > 2) {
 process.env.PORT = process.env.HABITICA_PORT || 3321
 process.env.NODE_DB_URI = process.env.HABITICA_DB_URI || 'mongodb://localhost/habitica-node-test'
 
+console.log('If running standalone, use the following command to set your environmental variables')
+console.log(`export PORT=${process.env.PORT}; export NODE_DB_URI='${process.env.NODE_DB_URI}'`)
 var server = require(`${PATH_TO_HABITICA}/website/server/`)
 
 server.listen(process.env.PORT, function () {
