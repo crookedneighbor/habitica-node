94b71da9af77a30e687b142c8ae7d7d64e4f8b83
Blade Barringer <blade@crookedneighbor.com>
2016-09-05 16:24:17 -0500

    chore: Switch from docco to jsdoc for docs

--- docs/assets/js/javascript.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('.ui.dropdown').dropdown()
-
-  $('.annotation pre').addClass('ui message')
-
-  $('#menu-buttons .button').popup()
-})
--- index.js
+++ index.js
@@ -1,98 +1,91 @@
-// Habitica API Wrapper
-// tasks
-// A thin wrapper around the Habitica API
 require('es6-promise').polyfill()
 var Connection = require('./lib/connection')
 var errors = require('./lib/errors')
 
-// # Habitica()
-// Set up your instance of Habitica.
-//
-// The endpoint will default to https://habitica.com/ if not provided.
-//
-// ```js
-// var Habitica = require('habitica')
-// var api = new Habitica({
-//   uuid: 'your-habitica.com-user-id',
-//   token: 'your-habitica.com-api-token',
-//   endpoint: 'http://custom-url.com'
-// })
-// ```
-//
-// The uuid and token parameters are not required and can be [set later](#setOptions) or be set at time of [registration](#register) or [login](#login).
-//
-// ```js
-// var Habitica = require('habitica')
-// var api = new Habitica()
-// ```
+/**
+ * @constructor
+ * @description Your client to interact with the [Habitica API](https://habitica.com/apidoc/).
+ *
+ * @param {Object} options - The properties to configure the Habitica client
+ * @param {String} [options.uuid] - The id of the user
+ * @param {String} [options.token] - The API token of the user
+ * @param {String} [options.endpoint=https://habitica.com/] - The endpoint to use
+ * @param {String} [options.platform=Habitica-Node] - The name of your integration
+ *
+ * @example
+ * var Habitica = require('habitica')
+ * var api = new Habitica({
+ *   uuid: 'your-habitica.com-user-id',
+ *   token: 'your-habitica.com-api-token',
+ *   endpoint: 'http://custom-url.com',
+ *   platform: 'The Name of Your Integration'
+ * })
+ * @example <caption>The uuid and token parameters are not required and can be set later. The credentials will be automatically set when using the register and localLogin methods.</caption>
+ * var Habitica = require('habitica')
+ * var api = new Habitica()
+ */
 function Habitica (options) {
   options = options || {}
 
   this._connection = new Connection(options)
 }
 
-// # getUuid()
-//
-// Gets the uuid of habitica instance.
-//
-// ```js
-// api.getUuid()
-// ```
+/** @public
+ * @returns {String} The user id used to make requests to the Habitica API
+ */
 Habitica.prototype.getUuid = function () {
   return this._connection.getUuid()
 }
 
-// # getToken()
-//
-// Gets the token of habitica instance.
-//
-// ```js
-// api.getToken()
-// ```
+/** @public
+ * @returns {String} The API token used to make requests to the Habitica API
+ */
 Habitica.prototype.getToken = function () {
   return this._connection.getToken()
 }
 
-// # getEndpoint()
-//
-// Gets the configured endpoint of habitica instance.
-//
-// ```js
-// api.getEndpoint()
-// ```
+/** @public
+ * @returns {String} The endpoint to make requests to
+ */
 Habitica.prototype.getEndpoint = function () {
   return this._connection.getEndpoint()
 }
 
-// # setOptions()
-//
-// Set credentials after initialization.
-//
-// If you do not provide a value, it will default to the previous value stored on initialization.
-//
-// ```js
-// api.setOptions({
-//   uuid: 'new-user-id',
-//   token: 'new-api-token',
-//   endpoint: 'http://localhost:3000/'
-// })
-// ```
+/** @public
+ *
+ * @param {Object} options - The properties to configure the Habitica client. If a property is not passed in, it will default to the value passed in on instantiation
+ * @param {String} [options.uuid] - The id of the user
+ * @param {String} [options.token] - The API token of the user
+ * @param {String} [options.endpoint] - The endpoint to use
+ * @param {String} [options.platform] - The name of your integration
+ *
+ * @example
+ * api.setOptions({
+ *   uuid: 'new-user-id',
+ *   token: 'new-api-token',
+ *   endpoint: 'http://localhost:3000/',
+ *   platform: 'Great-Habitica-Integration'
+ * })
+ */
 Habitica.prototype.setOptions = function (creds) {
   this._connection.setOptions(creds)
 }
 
-// # register()
-//
-// Registers a new account.
-//
-// The uuid and api token will be set automatically after a sucessful registration call.
-// ```js
-// api.register('username', 'email', 'password').then((res) => {
-//   var user = res.data
-// }).catch((err) => {
-//   // handle registration errors
-// })
-// ```
+/** @public
+ *
+ * @param {String} username - The username to register with
+ * @param {String} email - The email to register with
+ * @param {String} password - The password to register with
+ *
+ * @returns {Promise} A Promise that resolves the response from the register request
+ *
+ * @example <caption>The uuid and api token will be set automatically after a sucessful registration request</caption>
+ * api.register('username', 'email', 'password').then((res) => {
+ *   var user = res.data
+ * }).catch((err) => {
+ *   // handle registration errors
+ * })
+ */
 Habitica.prototype.register = function (username, email, password) {
   return this.post('/user/auth/local/register', {
     username: username,
@@ -109,24 +102,22 @@ Habitica.prototype.register = function (username, email, password) {
   }.bind(this))
 }
 
-// # localLogin()
-//
-// Logs into an existing account.
-//
-// You can log in with your username and password or your email and password.
-//
-// The uuid and api token will be set automatically after a sucessful login call.
-//
-// ```js
-// api.login('username or email','password').then((res) => {
-//   var creds = res.data
-//
-//   creds.id // the user's id
-//   creds.apiToken // the user's api token
-// }).catch((err) => {
-//   // handle login errors
-// })
-// ```
+/** @public
+ *
+ * @param {String} usernameOrEmail - The username or email to login with
+ * @param {String} password - The password to login with
+ *
+ * @returns {Promise} A Promise that resolves the response from the login request
+ * @example <caption>The uuid and api token will be set automatically after a sucessful login request</caption>
+ * api.login('username or email','password').then((res) => {
+ *   var creds = res.data
+ *
+ *   creds.id // the user's id
+ *   creds.apiToken // the user's api token
+ * }).catch((err) => {
+ *   // handle login errors
+ * })
+ */
 Habitica.prototype.localLogin = function (usernameEmail, password) {
   return this.post('/user/auth/local/login', {
     username: usernameEmail,
@@ -141,70 +132,72 @@ Habitica.prototype.localLogin = function (usernameEmail, password) {
   }.bind(this))
 }
 
-// # get
-//
-// Perform a GET request to Habitica's API.
-//
-// The first argument is the path. The second argument (optional) is an object that will be converted to a query string.
-//
-// ```js
-// api.get('/user').then((res) => {
-//   var user = res.data
-//
-//   user.profile.name // the user's display name
-// })
-//
-// api.get('/groups', {
-//   type: 'publicGuilds,privateGuilds'
-// }).then((res) => {
-//   var guilds = res.data
-//   var guild = guilds[0]
-//
-//   guild.name // the name of the group
-// })
-//
-// // error handling
-// api.get('/tasks/non-existant-id').then((res) => {
-//   // will never get here
-// }).catch((err) => {
-//   // handle task not found error
-//   err.message // 'Task not found'
-// })
-// ```
+/** @public
+ *
+ * @param {String} route - The Habitica API route to use
+ * @param {Object} [query] - Query params to send along with the request
+ *
+ * @returns {Promise} A Promise that resolves the response from the GET request
+ * @example <caption>Making a basic request</caption>
+ * api.get('/user').then((res) => {
+ *   var user = res.data
+ *
+ *   user.profile.name // the user's display name
+ * })
+ * @example <caption>A request with a query Object</caption>
+ * api.get('/groups', {
+ *   type: 'publicGuilds,privateGuilds'
+ * }).then((res) => {
+ *   var guilds = res.data
+ *   var guild = guilds[0]
+ *
+ *   guild.name // the name of the group
+ * })
+ *
+ * @example <caption>Handling errors</caption>
+ * api.get('/tasks/non-existant-id').then((res) => {
+ *   // will never get here
+ * }).catch((err) => {
+ *   err.message // 'Task not found'
+ * })
+ */
 Habitica.prototype.get = function (path, query) {
   return this._connection.get(path, {
     query: query
   })
 }
 
-// # post
-//
-// Perform a POST request to Habitica's API.
-//
-// The first argument is the path. The second argument (optional) is an object that will be sent as the body of the POST request. The thrid argument (optional) is an object that will be converted to a query string.
-//
-// ```js
-// api.post('/tasks/user', {
-//   text: 'Task Name',
-//   notes: 'Task Notes',
-//   type: 'todo'
-// }).then((res) => {
-//   var task = res.data
-//
-//   task.text // 'Task Name'
-// })
-//
-// api.post('/groups', {
-//   type: 'party',
-//   name: 'My Party'
-// }).then((res) => {
-//   var party = res.data
-//
-//   party.name // 'My Party'
-// }).catch((err) => {
-//   // handle errors
-// })
-// ```
+/** @public
+ *
+ * @param {String} route - The Habitica API route to use
+ * @param {Object} [body] - The body to send along with the request
+ * @param {Object} [query] - Query params to send along with the request
+ *
+ * @returns {Promise} A Promise that resolves the response from the POST request
+ *
+ * @example <caption>A request with a body</caption>
+ * api.post('/tasks/user', {
+ *   text: 'Task Name',
+ *   notes: 'Task Notes',
+ *   type: 'todo'
+ * }).then((res) => {
+ *   var task = res.data
+ *
+ *   task.text // 'Task Name'
+ * })
+ *
+ * @example <caption>Handling errors</caption>
+ * api.post('/groups', {
+ *   type: 'party',
+ *   name: 'My Party'
+ * }).then((res) => {
+ *   var party = res.data
+ *
+ *   party.name // 'My Party'
+ * }).catch((err) => {
+ *   // handle errors
+ * })
+ */
 Habitica.prototype.post = function (path, body, query) {
   return this._connection.post(path, {
     send: body,
@@ -212,32 +205,35 @@ Habitica.prototype.post = function (path, body, query) {
   })
 }
 
-// # put
-//
-// Perform a PUT request to Habitica's API.
-//
-// The first argument is the path. The second argument (optional) is an object that will be sent as the body of the PUT request. The thrid argument (optional) is an object that will be converted to a query string.
-//
-// ```js
-// api.put('/tasks/the-task-id', {
-//   text: 'New Task Name',
-//   notes: 'New Text Notes'
-// }).then((res) => {
-//   var task = res.data
-//
-//   task.text // 'New Task Name'
-// })
-//
-// api.put('/groups/the-group-id', {
-//   name: 'New Group Name'
-// }).then((res) => {
-//   var group = res.data
-//
-//   group.name // 'New Group Name'
-// }).catch((err) => {
-//   // handle errors
-// })
-// ```
+/** @public
+ *
+ * @param {String} route - The Habitica API route to use
+ * @param {Object} [body] - The body to send along with the request
+ * @param {Object} [query] - Query params to send along with the request
+ *
+ * @returns {Promise} A Promise that resolves the response from the PUT request
+ *
+ * @example <caption>A request with a body</caption>
+ * api.put('/tasks/the-task-id', {
+ *   text: 'New Task Name',
+ *   notes: 'New Text Notes'
+ * }).then((res) => {
+ *   var task = res.data
+ *
+ *   task.text // 'New Task Name'
+ * })
+ *
+ * @example <caption>Handling errors</caption>
+ * api.put('/groups/the-group-id', {
+ *   name: 'New Group Name'
+ * }).then((res) => {
+ *   var group = res.data
+ *
+ *   group.name // 'New Group Name'
+ * }).catch((err) => {
+ *   // handle errors
+ * })
+ */
 Habitica.prototype.put = function (path, body, query) {
   return this._connection.put(path, {
     send: body,
@@ -245,23 +241,26 @@ Habitica.prototype.put = function (path, body, query) {
   })
 }
 
-// # del
-//
-// Perform a DELETE request to Habitica's API.
-//
-// The first argument is the path. The second argument (optional) is an object that will be sent as the body of the DELETE request. The thrid argument (optional) is an object that will be converted to a query string.
-//
-// ```js
-// api.del('/tasks/the-task-id').then(() => {
-//   // The task has been deleted
-// })
-//
-// api.del('/groups/the-group-id').then(() => {
-//  // The group has been deleted
-// }).catch((err) => {
-//   // handle errors
-// })
-// ```
+/** @public
+ *
+ * @param {String} route - The Habitica API route to use
+ * @param {Object} [body] - The body to send along with the request
+ * @param {Object} [query] - Query params to send along with the request
+ *
+ * @returns {Promise} A Promise that resolves the response from the DELETE request
+ *
+ * @example <caption>A basic request</caption>
+ * api.del('/tasks/the-task-id').then(() => {
+ *   // The task has been deleted
+ * })
+ *
+ * @example <caption>Handling errors</caption>
+ * api.del('/groups/the-group-id').then(() => {
+ *  // The group has been deleted
+ * }).catch((err) => {
+ *   // handle errors
+ * })
+ */
 Habitica.prototype.del = function (path, body, query) {
   return this._connection.del(path, {
     send: body,
@@ -269,7 +268,7 @@ Habitica.prototype.del = function (path, body, query) {
   })
 }
 
-// And you're ready!
 Habitica.ApiError = errors.HabiticaApiError
 Habitica.UnknownConnectionError = errors.UnknownConnectionError
+
 module.exports = Habitica
